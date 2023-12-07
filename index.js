911ae1daaffbb8e84d0b9cb93f1288efce90f848
Felipe Limas <dafevi.limas@gmail.com>
2023-12-06 23:34:15 -0500

    listo para upload

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
          //Observador de interseccion
          const opcion = {
-            root:null, 
-            threshold:1.0
-           } 
-           let objetivoOculto = document.querySelectorAll('.targets');
-           let interseccionObsevada = new IntersectionObserver(
-            entries=>{
-              entries.forEach((entry)=>{
-                  entry.target.classList.toggle('showObserver',entry.isIntersecting);
-              })
-            }, opcion);
-           objetivoOculto.forEach((seccion)=>interseccionObsevada.observe(seccion)); 
-            //Texto dinamico
-            setInterval(()=>{
-              const owner = 'Don Felipe Limas';
-              document.getElementById('textDinamic').innerText = `Esta web la realizo ${owner}`;
-            },3000); 
-            //Escritura de texto en pantalla
-            const text = document.getElementById('section1');
-            let str = text.innerHTML;
-            text.innerHTML= "";
-            let i = 0;
-            const speed = 100;
-            const typeText = ()=> {
-              if(i < str.length) {
-                text.innerHTML += str.charAt(i);
-                i++;
-                setTimeout(typeText,speed);
-              }
-            };
-            setInterval(() => {
+          root:null, 
+          threshold:1.0
+         } 
+         let objetivoOculto = document.querySelectorAll('.targets');
+         let interseccionObsevada = new IntersectionObserver(
+          entries=>{
+            entries.forEach((entry)=>{
+                entry.target.classList.toggle('showObserver',entry.isIntersecting);
+            })
+          }, opcion);
+         objetivoOculto.forEach((seccion)=>interseccionObsevada.observe(seccion)); 
+          //Texto dinamico
+          setInterval(()=>{
+            const owner = 'Don Felipe Limas';
+            document.getElementById('textDinamic').innerText = `Esta web la realizo ${owner}`;
+          },3000); 
+          //Escritura de texto en pantalla
+          const text = document.getElementById('section1');
+          let str = text.innerHTML;
+          text.innerHTML= "";
+          let i = 0;
+          const speed = 100;
+          const typeText = ()=> {
+            if(i < str.length) {
+              text.innerHTML += str.charAt(i);
+              i++;
               setTimeout(typeText,speed);
-            }, 3000);
+            }
+          };
+          setInterval(() => {
+            setTimeout(typeText,speed);
+          }, 3000);
