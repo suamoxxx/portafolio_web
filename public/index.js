0c33abe1ef4deb2f0fafe7e3701493ad08bc3c9c
Felipe Limas <dafevi.limas@gmail.com>
2024-08-16 00:12:49 -0500

    Restaurar, copia uno.

--- public/index.js
+++ public/index.js
@@ -1,31 +1,28 @@
+const opcion = {
+      root: document,
+      rootMargin: "0px",
+      threshold: 0.7
+ } 
+let objetivoOculto = document.querySelectorAll('.targets');
+let interseccionObsevada = new IntersectionObserver(
+        entries=>{
+            entries.forEach((entry)=>{
+               entry.target.classList.toggle('showObserver',entry.isIntersecting);
+            }
+         )}, opcion
+);
+objetivoOculto.forEach((seccion)=>interseccionObsevada.observe(seccion));            
+function txtdyn(){
+        setInterval(()=>{
+          const owner = 'Don Felipe Limas';
+          document.getElementById('textDinamic').innerText = `Esta web la realizo ${owner}`;
+        },9000); 
+}
+txtdyn();
 
-        const opcion = {
-          root: document,
-          rootMargin: "0px",
-          threshold: 0.7
-        } 
-        let objetivoOculto = document.querySelectorAll('.targets');
-        let interseccionObsevada = new IntersectionObserver(
-            entries=>{
-              entries.forEach((entry)=>{
-                  entry.target.classList.toggle('showObserver',entry.isIntersecting);
-              })
-            }, opcion);
+document.getElementById('btn_one').addEventListener('click', ()=>{
+  const text = document.getElementById('p2-sec1');
+  console.log(typeof(text))
+})
 
-        objetivoOculto.forEach((seccion)=>interseccionObsevada.observe(seccion)); 
-           
-         function txtdyn(){
-            setInterval(()=>{
-            const owner = 'Don Felipe Limas';
-            document.getElementById('textDinamic').innerText = `Esta web la realizo ${owner}`;
-            },9000); 
-        }
 
-        txtdyn();
-
- 
-          const btn = document.getElementById('btn_contact2');
-          btn.addEventListener("click", ()=> {
-          window.location.href = "contacto.html";
-          });
-      
