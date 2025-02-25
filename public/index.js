fa2ba481c9585d43d4b790603058be76ba363fcf
David Felipe Villamarin <dafevi.limas@gmail.com>
2025-02-25 02:07:23 -0500

    Se inicia a desarrollar media querys para otras pantallas

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-// Configuración Observador de interseccion
+// Configuración Observador de interseccion con la API de Intersection Observer
 const opcion = {
   root:null, 
   threshold:0.9
@@ -19,9 +19,9 @@ setInterval(()=>{
       document.getElementById('textDinamic').innerText = `Esta web la realizo ${owner}`;
 },3000); 
 
-//Escritura de texto en pantalla
+//Escritura tipea texto en pantalla usando charAt
 const text = document.getElementById('p2-sec1');
-let str = text.innerHTML;//Valor del string
+let str = text.innerHTML;
 text.innerHTML= "";
 let i = 0;
 const speed = 100;
@@ -35,3 +35,52 @@ const typeText = ()=> {
 setInterval(() => {
      setTimeout(typeText,speed);
 }, 3000);
+
+// Enviar correo al backend
+const submitForm = document.getElementById('form-contact')
+submitForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  const n = document.getElementById('name');
+  const email = document.getElementById('email');
+  const m = document.getElementById('message');
+  const name = n.value;
+  const mail = email.value; 
+  const message = m.value;
+  const response = fetch('http://127.0.0.1:3001/mailer', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+      body: JSON.stringify({ name, mail, message })
+    }); 
+    n.value = ''
+    email.value = ''
+    m.value = ''
+    
+})
+
+// Muestra el modal del portafolio
+const btnModal = document.getElementById("btn_one")
+btnModal.addEventListener("click", (e)=>{
+  e.preventDefault()
+  const elements = document.querySelectorAll('.modal-hidden');
+  elements.forEach(element=> {
+      element.style.opacity= 0
+      document.getElementById("form-modal").style.opacity = 1
+      document.getElementById("btn_one").style.display = "none"
+      document.getElementById("sec1").style.backgroundColor = "#424242"      
+  })
+})
+
+// Cierre del modal 
+const closeModal = document.getElementById("closed-modal")
+closeModal.addEventListener('click',(e)=>{
+  e.preventDefault()
+  const elements = document.querySelectorAll('.modal-hidden');
+  elements.forEach(element=> {
+      element.style.opacity= 1
+      document.getElementById("form-modal").style.opacity = 0
+      document.getElementById("btn_one").style.display = "block"
+      document.getElementById("sec1").style.backgroundColor = "white"
+      typeText()
+
+  })
+})
--- server.js
+++ server.js
@@ -3,22 +3,59 @@ import path from 'path';
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import nodemailer from 'nodemailer'
-
-const app = express();
-app.set('port', process.env.PORT || 3001)
-
+import cors from 'cors'
+import morgan from 'morgan';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const corsOptions = {
+    origin: 'http://127.0.0.1:3001',
+    methods: ['GET', 'POST'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
+}
+const app = express();
+app.set('port', process.env.PORT || 3001)
+app.use(morgan('dev'));
 
+app.use(cors(corsOptions))
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.json())
 app.get('/portafolio', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.post('/sendMailer', (res,req)=>{
-    const { name, email, message } = req.body
-    
+// Api Rest Post para envio de datos al correo
+app.post('/mailer', async (req,res)=>{
+    const { name, email, message } = req.body;
+    try {
+        // Configuracion del smtp 
+        const transport = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, 
+            auth: {
+              user: "dafevi.limas@gmail.com",
+              pass: "ttklqjokolnxljwt",
+            },
+        });
+        // Mensaje a enviar
+        let mail = { 
+            from: `${email}`,
+            to: "dafevi.limas@gmail.com",
+            subject: "Mensaje desde el portafolio",
+            text: ` Hola me llamo ${name},\n ${message}`
+        };
+       await transport.sendMail(mail,(error, info)=>{
+            if(error){
+                console.log(error)
+                return res.status(500).json({ error: 'Error al enviar el correo' });
+            } else {
+                console.log("mail send")
+                return res.status(200).json({ message: 'Correo enviado correctamente' });
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 app.listen(app.get('port'), () => {
     console.log(`Server running on port ${app.get('port')}`);
